55302b890ea3f71e44d77e32c86dcff3131a5c49
Fajril Ambia <26703327+fajril@users.noreply.github.com>
2023-07-02 08:59:03 +0700

    add test for decline balance depreciation

--- tests/tests_depreciation.py
+++ tests/tests_depreciation.py
@@ -25,3 +25,30 @@ def test_straight_line_depreciation_rate():
 
     np.testing.assert_array_almost_equal(depreciation_charge, depreciation_charge_calc)
     np.testing.assert_array_almost_equal(book_value, book_value_calc)
+
+def test_decline_balance_depreciation():
+    """ Test for Decline balance Depreciation.
+    In this test we use decline factor = 2
+    """
+
+    item_cost = 200_000
+    salvage_value = 25_000
+    useful_life = 5
+
+    depreciation_charge = []
+    book_value = []
+
+    depreciation_charge_calc = psc.decline_balance_depreciation_rate(
+        cost=item_cost,
+        salvage_value=salvage_value,
+        useful_life=useful_life,
+    )
+
+    book_value_calc = psc.decline_balance_book_value(
+        cost=item_cost,
+        salvage_value=salvage_value,
+        useful_life=useful_life
+    )
+
+    np.testing.assert_array_almost_equal(depreciation_charge, depreciation_charge_calc)
+    np.testing.assert_array_almost_equal(book_value, book_value_calc)
