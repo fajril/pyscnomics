37f3a94c49c3851927251ea60189d4d3e57780f9
adhimmulia <adhimmuliam@gmail.com>
2023-10-30 08:55:46 +0700

    Adding transition python file ada dataset
    Adding transition python file and dataset

---
+++ pyscnomics/contracts/transition.py
@@ -0,0 +1,23 @@
+import numpy as np
+import datetime
+from pyscnomics.econ.revenue import Lifting
+from pyscnomics.econ.costs import Tangible, Intangible, OPEX, ASR
+from pyscnomics.contracts.costrecovery import CostRecovery
+from pyscnomics.contracts.grossplit import GrossSplit
+
+
+# Modifying the Lifting attributes into the current updates project length, fill it with zeros
+
+# Modifying the Tangible, Intangible, OPEX, ASR attributes into the current updates project length, fill it with zeros
+
+# Get the proportion of the days of contract transition to a year
+
+# Modify the latest row of Lifting, Tangible, Intangible, OPEX, ASR into the proportioned value
+
+# Run each PSC
+
+# Sum the generated cashflow
+
+def transition(instance):
+
+    return NotImplemented
