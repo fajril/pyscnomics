b3ca037aced70d885620c7e0115c96139952eaae
adhimmulia <adhimmuliam@gmail.com>
2023-12-13 15:44:01 +0700

    Adding POC of excel bridge
    Adding POC of excel bridge. Now could communicate in two way between excel and python

---
+++ main.py
@@ -0,0 +1,31 @@
+"""
+Python Script as the entry point of Excel Workbook
+"""
+import xlwings as xw
+
+
+def main(workbook_path, mode):
+    # Defining the workbook object
+    ws = xw.Book(workbook_path).sheets('Result')
+
+    if mode == 'Standard':
+        result = 'Standard Result'
+    else:
+        result = mode
+
+    # Test Write
+    ws.range("B30").value = 'Test Run Python from Excel ' + result
+
+    # Giving the workbook execution status
+    ws.range("C24").value = 'Success'
+
+
+if __name__ == '__main__':
+    import sys
+
+    main(workbook_path=sys.argv[1], mode=sys.argv[2])
+    #
+    # workbook_path = 'Test Entry Point.xlsb'
+    # run_mode = 'Standard'
+    #
+    # main(workbook_path=workbook_path, mode=run_mode)
