e14eedb4bb024ec8827424faf5223ef3ddf5bb4a
Aditya Dewanto Hartono <adityadewanto@gmail.com>
2025-08-29 18:28:17 +0700

    Add a fraction of unit testings for BaseProject

--- tests/test_base_project_cost_total.py
+++ tests/test_base_project_cost_total.py
@@ -1,172 +1,193 @@
 """
-A collection of unit testings to validate operations occurred
-in class BaseProject: sunk cost
+A collection of unit testigs to validate calculations
+involving total cost categories
 """
 
 from datetime import date
 import numpy as np
 
 from pyscnomics.econ.selection import OtherRevenue, FluidType, CostType
-from pyscnomics.econ.revenue import Lifting
 from pyscnomics.econ.costs import CapitalCost, Intangible, OPEX, ASR, LBT, CostOfSales
 from pyscnomics.contracts.project import BaseProject
 
 
-# Synthetic
-params_base = {
-    "sulfur_revenue": OtherRevenue.REDUCTION_TO_OIL_OPEX,
-    "electricity_revenue": OtherRevenue.ADDITION_TO_OIL_REVENUE,
-    "co2_revenue": OtherRevenue.ADDITION_TO_GAS_REVENUE,
-    "tax_rate": 0.0,
-    "year_inflation": None,
-    "inflation_rate": 0.0,
-    "inflation_rate_applied_to": None,
+kwargs = {
+    "start_date": date(year=2023, month=1, day=1),
+    "end_date": date(year=2034, month=12, day=31),
+    "oil_onstream_date": date(year=2030, month=1, day=1),
+    "gas_onstream_date": date(year=2029, month=1, day=1),
+    "approval_year": 2027,
 }
 
 
-def test_base_project_get_oil_lifting():
+def test_capital_cost_total():
 
-    # Synthetic Data Lifting
-    oil_lifting = Lifting(
+    # Instance #1
+    capital_mangga = CapitalCost(
         start_year=2023,
         end_year=2034,
-        prod_year=np.array([2030, 2031, 2032, 2033, 2034]),
-        lifting_rate=np.array([100, 100, 100, 100, 100]),
-        price=np.array([10, 10, 10, 10, 10]),
-        fluid_type=FluidType.OIL,
+        expense_year=np.array(
+            [
+                2023, 2024, 2025, 2026, 2027, 2028,
+                2029, 2030, 2031, 2032, 2033, 2034,
+            ]
+        ),
+        cost=np.array(
+            [
+                200, 200, 200, 200, 150, 100,
+                100, 75, 50, 50, 50, 50
+            ]
+        ),
+        cost_allocation=(
+            [
+                FluidType.OIL, FluidType.OIL, FluidType.OIL, FluidType.OIL,
+                FluidType.OIL, FluidType.OIL, FluidType.OIL, FluidType.OIL,
+                FluidType.OIL, FluidType.OIL, FluidType.OIL, FluidType.OIL,
+            ]
+        ),
     )
 
-    gas_lifting = Lifting(
+    # Instance #2
+    capital_apel = CapitalCost(
         start_year=2023,
         end_year=2034,
-        prod_year=np.array([2030, 2031, 2032, 2033, 2029]),
-        lifting_rate=np.array([10, 10, 10, 10, 10]),
-        price=np.array([1, 1, 1, 1, 1]),
-        fluid_type=FluidType.GAS,
+        expense_year=np.array(
+            [
+                2023, 2024, 2025, 2026, 2027, 2028,
+                2029, 2030, 2031, 2032, 2033, 2034,
+            ]
+        ),
+        cost=np.array(
+            [
+                200, 200, 200, 200, 150, 100,
+                100, 75, 50, 50, 50, 50
+            ]
+        ),
+        cost_allocation=(
+            [
+                FluidType.GAS, FluidType.GAS, FluidType.GAS, FluidType.GAS,
+                FluidType.GAS, FluidType.GAS, FluidType.GAS, FluidType.GAS,
+                FluidType.GAS, FluidType.GAS, FluidType.GAS, FluidType.GAS,
+            ]
+        ),
     )
 
-    total_lifting = tuple([oil_lifting, gas_lifting])
+    capital_total = tuple([capital_mangga, capital_apel])
 
-    pr = BaseProject(
-        start_date=date(year=2023, month=1, day=1),
-        end_date=date(year=2034, month=12, day=31),
-        oil_onstream_date=date(year=2030, month=1, day=1),
-        gas_onstream_date=date(year=2029, month=1, day=1),
-        approval_year=2027,
-        lifting=total_lifting,
+    # Calculated results
+    pr = BaseProject(**kwargs, capital_cost=capital_total)
+    calculated = {
+        "expense_year": pr.capital_cost_total.expense_year,
+        "cost": pr.capital_cost_total.cost,
+    }
+
+    # Expected results
+    expected = {
+        "expense_year": np.array(
+            [
+                2023, 2024, 2025, 2026, 2027, 2028, 2029, 2030, 2031, 2032, 2033, 2034,
+                2023, 2024, 2025, 2026, 2027, 2028, 2029, 2030, 2031, 2032, 2033, 2034,
+            ]
+        ),
+        "cost": np.array(
+            [
+                200, 200, 200, 200, 150, 100, 100, 75, 50, 50, 50, 50,
+                200, 200, 200, 200, 150, 100, 100, 75, 50, 50, 50, 50,
+            ]
+        ),
+    }
+
+    # Execute testings
+    np.testing.assert_allclose(calculated["expense_year"], expected["expense_year"])
+    np.testing.assert_allclose(calculated["cost"], expected["cost"])
+
+
+def test_intangible_cost_total():
+
+    # Instance #1
+    intangible_mangga = Intangible(
+        start_year=2023,
+        end_year=2034,
+        expense_year=np.array(
+            [
+                2023, 2024, 2025, 2026, 2027, 2028,
+                2029, 2030, 2031, 2032, 2033, 2034,
+            ]
+        ),
+        cost=np.array(
+            [
+                200, 200, 200, 200, 150, 100,
+                100, 75, 50, 50, 50, 50
+            ]
+        ),
+        cost_allocation=(
+            [
+                FluidType.OIL, FluidType.OIL, FluidType.OIL, FluidType.OIL,
+                FluidType.OIL, FluidType.OIL, FluidType.OIL, FluidType.OIL,
+                FluidType.OIL, FluidType.OIL, FluidType.OIL, FluidType.OIL,
+            ]
+        ),
     )
+
+    # Instance #2
+    intangible_apel = Intangible(
+        start_year=2023,
+        end_year=2034,
+        expense_year=np.array(
+            [
+                2023, 2024, 2025, 2026, 2027, 2028,
+                2029, 2030, 2031, 2032, 2033, 2034,
+            ]
+        ),
+        cost=np.array(
+            [
+                200, 200, 200, 200, 150, 100,
+                100, 75, 50, 50, 50, 50
+            ]
+        ),
+        cost_allocation=(
+            [
+                FluidType.GAS, FluidType.GAS, FluidType.GAS, FluidType.GAS,
+                FluidType.GAS, FluidType.GAS, FluidType.GAS, FluidType.GAS,
+                FluidType.GAS, FluidType.GAS, FluidType.GAS, FluidType.GAS,
+            ]
+        ),
+    )
+
+    intangible_total = tuple([intangible_mangga, intangible_apel])
+
+    # Calculated results
+    pr = BaseProject(**kwargs, intangible_cost=intangible_total)
 
     calculated = {
-        "lifting_rate": pr._get_oil_lifting().lifting_rate,
-        "price": pr._get_oil_lifting().price,
+        "expense_year": pr.intangible_cost_total.expense_year,
+        "cost": pr.intangible_cost_total.cost,
     }
 
+    # Expected results
     expected = {
-        "lifting_rate": np.array([100, 100, 100, 100, 100]),
-        "price": np.array([10, 10, 10, 10, 10]),
+        "expense_year": np.array(
+            [
+                2023, 2024, 2025, 2026, 2027, 2028, 2029, 2030, 2031, 2032, 2033, 2034,
+                2023, 2024, 2025, 2026, 2027, 2028, 2029, 2030, 2031, 2032, 2033, 2034,
+            ]
+        ),
+        "cost": np.array(
+            [
+                200, 200, 200, 200, 150, 100, 100, 75, 50, 50, 50, 50,
+                200, 200, 200, 200, 150, 100, 100, 75, 50, 50, 50, 50,
+            ]
+        ),
     }
 
-    np.testing.assert_allclose(calculated["lifting_rate"], expected["lifting_rate"])
-    np.testing.assert_allclose(calculated["price"], expected["price"])
+    # Execute testings
+    np.testing.assert_allclose(calculated["expense_year"], expected["expense_year"])
+    np.testing.assert_allclose(calculated["cost"], expected["cost"])
 
 
-def test_base_project_get_gas_lifting():
+def test_lbt_cost_total():
+    pass
 
-    # Synthetic Data Lifting
-    oil_lifting = Lifting(
-        start_year=2023,
-        end_year=2034,
-        prod_year=np.array([2030, 2031, 2032, 2033, 2034]),
-        lifting_rate=np.array([100, 100, 100, 100, 100]),
-        price=np.array([10, 10, 10, 10, 10]),
-        fluid_type=FluidType.OIL,
-    )
-
-    gas_lifting = Lifting(
-        start_year=2023,
-        end_year=2034,
-        prod_year=np.array([2030, 2031, 2032, 2033, 2029]),
-        lifting_rate=np.array([10, 10, 10, 10, 10]),
-        price=np.array([1, 1, 1, 1, 1]),
-        fluid_type=FluidType.GAS,
-    )
-
-    total_lifting = tuple([oil_lifting, gas_lifting])
-
-    pr = BaseProject(
-        start_date=date(year=2023, month=1, day=1),
-        end_date=date(year=2034, month=12, day=31),
-        oil_onstream_date=date(year=2030, month=1, day=1),
-        gas_onstream_date=date(year=2029, month=1, day=1),
-        approval_year=2027,
-        lifting=total_lifting,
-    )
-
-    calculated = {
-        "lifting_rate": pr._get_gas_lifting().lifting_rate,
-        "price": pr._get_gas_lifting().price,
-    }
-    expected = {
-        "lifting_rate": np.array([10, 10, 10, 10, 10]),
-        "price": np.array([1, 1, 1, 1, 1]),
-    }
-
-    np.testing.assert_allclose(calculated["lifting_rate"], expected["lifting_rate"])
-    np.testing.assert_allclose(calculated["price"], expected["price"])
-
-
-def test_base_project_get_sulfur_lifting():
-
-    # Synthetic Data Lifting
-    oil_lifting = Lifting(
-        start_year=2023,
-        end_year=2034,
-        prod_year=np.array([2030, 2031, 2032, 2033, 2034]),
-        lifting_rate=np.array([100, 100, 100, 100, 100]),
-        price=np.array([10, 10, 10, 10, 10]),
-        fluid_type=FluidType.OIL,
-    )
-
-    gas_lifting = Lifting(
-        start_year=2023,
-        end_year=2034,
-        prod_year=np.array([2030, 2031, 2032, 2033, 2029]),
-        lifting_rate=np.array([10, 10, 10, 10, 10]),
-        price=np.array([1, 1, 1, 1, 1]),
-        fluid_type=FluidType.GAS,
-    )
-
-    sulfur_lifting = Lifting(
-        start_year=2023,
-        end_year=2034,
-        prod_year=np.array([2030, 2031, 2032, 2033, 2029]),
-        lifting_rate=np.array([5, 5, 5, 5, 5]),
-        price=np.array([0.1, 0.1, 0.1, 0.1, 0.1]),
-        fluid_type=FluidType.SULFUR,
-    )
-
-    total_lifting = tuple([oil_lifting, gas_lifting, sulfur_lifting])
-
-    pr = BaseProject(
-        start_date=date(year=2023, month=1, day=1),
-        end_date=date(year=2034, month=12, day=31),
-        oil_onstream_date=date(year=2030, month=1, day=1),
-        gas_onstream_date=date(year=2029, month=1, day=1),
-        approval_year=2027,
-        lifting=total_lifting,
-    )
-
-    calculated = {
-        "lifting_rate": pr._get_sulfur_lifting().lifting_rate,
-        "price": pr._get_sulfur_lifting().price,
-    }
-    expected = {
-        "lifting_rate": np.array([5, 5, 5, 5, 5]),
-        "price": np.array([0.1, 0.1, 0.1, 0.1, 0.1]),
-    }
-
-    np.testing.assert_allclose(calculated["lifting_rate"], expected["lifting_rate"])
-    np.testing.assert_allclose(calculated["price"], expected["price"])
 
 
 def test_classify_capital_cost_by_fluid_1():
