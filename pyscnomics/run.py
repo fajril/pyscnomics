1b20b57035f9cae17ae141877db84fa22fd69246
Aditya Dewanto Hartono <adityadewanto@gmail.com>
2025-10-02 10:39:31 +0700

    Update project.py

--- pyscnomics/run.py
+++ pyscnomics/run.py
@@ -52,7 +52,7 @@ kwargs_pr = {
     "oil_onstream_date": date(year=2030, month=1, day=1),
     "gas_onstream_date": date(year=2029, month=1, day=1),
     "approval_year": 2026,
-    "is_pod_1": True,
+    "is_pod_1": False,
 }
 
 params_base = {
