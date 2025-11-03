23bdf5cc066d944960a5181126859926f25b8b52
Aditya Dewanto Hartono <adityadewanto@gmail.com>
2025-11-03 22:12:31 +0700

    Update database.py

--- pyscnomics/run.py
+++ pyscnomics/run.py
@@ -9,6 +9,7 @@ from pyscnomics.poolData import (
     synthetic_data_dict,
     optimization_arguments_dict,
 )
+from pyscnomics.dataset.poolData1 import PrepareLiftingCostsAsClass
 from pyscnomics.econ.selection import (
     OptimizationParameter,
     OptimizationTarget,
@@ -23,20 +24,21 @@ from pyscnomics.optimize.optimization import (
 
 if __name__ == "__main__":
 
-    cr, gs, bp = "cost_recovery", "gross_split", "base_project"
+    # cr, gs, bp = "cost_recovery", "gross_split", "base_project"
+    #
+    # # Specify contract type
+    # contract = gs
+    # contract_arguments = get_contract_arguments_class(contract)
+    # summary_arguments = get_summary_arguments_class()
+    # optimization_arguments = optimization_arguments_dict()
+    #
+    # # Generate synthetic data
+    # contract_as_class = synthetic_data_class(contract)
+    # contract_as_dict = synthetic_data_dict(contract)
 
-    # Specify contract type
-    contract = gs
-    contract_arguments = get_contract_arguments_class(contract)
-    summary_arguments = get_summary_arguments_class()
-    optimization_arguments = optimization_arguments_dict()
+    data = PrepareLiftingCostsAsClass()
 
-    # Generate synthetic data
-    contract_as_class = synthetic_data_class(contract)
-    contract_as_dict = synthetic_data_dict(contract)
-
-    contract_as_class.run(**contract_arguments)
-    t1 = contract_as_class.get_summary(**summary_arguments)
+    t1 = data.cost_of_sales
 
     # print('\t')
     # print(f'Filetype: {type()}')
@@ -46,4 +48,4 @@ if __name__ == "__main__":
     print('\t')
     print(f'Filetype: {type(t1)}')
     print(f'Length: {len(t1)}')
-    print('t1 = \n', t1["ctr_irr"])
+    print('t1 = \n', t1)
